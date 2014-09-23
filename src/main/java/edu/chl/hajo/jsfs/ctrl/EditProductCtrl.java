94822f51d34e37b19a66aa4cd172cba6a91294de
Tomas Hasselquist <tomas@hasselquist.se>
2014-09-23 16:38:24 +0200

    Added methods for edit product (Ctrl and BB)

--- src/main/java/edu/chl/hajo/jsfs/ctrl/EditProductCtrl.java
+++ src/main/java/edu/chl/hajo/jsfs/ctrl/EditProductCtrl.java
@@ -1,11 +1,71 @@
 package edu.chl.hajo.jsfs.ctrl;
 
+import edu.chl.hajo.jsfs.core.SingletonShop;
+import edu.chl.hajo.jsfs.view.EditProductBB;
+import edu.chl.hajo.shop.core.Product;
+import edu.chl.hajo.shop.core.Shop;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
 /**
  *
  * @author hajo
  */
-
+@Named
+@RequestScoped
 public class EditProductCtrl {
+    private static final Logger LOG = Logger.getLogger(EditProductCtrl.class.getName());
+    private Shop reg;
+    private EditProductBB productBB;
 
+    protected EditProductCtrl() {
+        // Must have for CDI
+    }
+
+    @PostConstruct
+    public void post() {
+        LOG.log(Level.INFO, "PersonDetailCtrl alive {0}", this);
+    }
+
+    @PreDestroy
+    public void pre() {
+        LOG.log(Level.INFO, "PersonDetailCtrl to be destroyed {0}", this);
+    }
+
+    @Inject
+    public EditProductCtrl(SingletonShop reg) {
+        this.reg = (Shop) reg.getShop();
+    }
+    
+    // Lie this --------------------
+    @Inject
+    public void setProductBB(EditProductBB productBB) {
+        this.productBB = productBB;
+    }
+   
+    /*public String save() {
+        LOG.log(Level.INFO, "Save: {0}" + productBB);
+        reg.getProductCatalogue().update(new Product(productBB.getId(), productBB.getName(), productBB.getPrice()));
+        return "productList?faces-redirect=true";
+    }*/
+    // ---- or like this 
+    /*public String save(PersonDetailBB personBB) {
+        LOG.log(Level.INFO, "Save: {0}" + personBB);
+        reg.create(new Person(personBB.getId(), personBB.getFname(), personBB.getAge()));
+        return "personList?faces-redirect=true";
+    }*/
+    
+    
+
+    public String update() {
+        LOG.log(Level.INFO, "Update: {0}" + productBB);
+        reg.getProductCatalogue().update(new Product(productBB.getId(), productBB.getName(), productBB.getPrice()));
+        return "productList?faces-redirect=true";
+    }
   
 }
--- src/main/java/edu/chl/hajo/jsfs/view/EditProductBB.java
+++ src/main/java/edu/chl/hajo/jsfs/view/EditProductBB.java
@@ -1,12 +1,69 @@
 
 package edu.chl.hajo.jsfs.view;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
 /**
  *
  * @author hajo
  */
 
+@Named
+@ViewScoped
 public class EditProductBB  {
+    
+    private static final Logger LOG = Logger.getLogger(EditProductBB.class.getName());
 
+    private Long id;
+    private String name;
+    private double price;
+
+    @PostConstruct
+    public void post() {
+        LOG.log(Level.INFO, "PersonDetailBB alive {0}", this);
+    }
+
+    @PreDestroy
+    public void pre() {
+        LOG.log(Level.INFO, "PersonDetailBB to be destroyed {0}", this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+        LOG.log(Level.INFO, "Set id {0}", id);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        LOG.log(Level.INFO, "Set fname {0}", name);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+        LOG.log(Level.INFO, "Set age {0}", price);
+    }
+
+    @Override
+    public String toString() {
+        return "PersonDetailBB{" + "id=" + id + ", fname=" + name + ", age=" + price + '}';
+    }
   
 }
